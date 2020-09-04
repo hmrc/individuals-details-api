ba230234713ea3240a8761110cb3fb523dde70c4
ajchilts <40767309+ajchilts@users.noreply.github.com>
2020-09-04 11:06:25 +0100

    Update confidence level on auth and small refactor

--- app/uk/gov/hmrc/individualsdetailsapi/controllers/APIDocumentationController.scala
+++ app/uk/gov/hmrc/individualsdetailsapi/controllers/APIDocumentationController.scala
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package uk.gov.hmrc.individualsdetailsapi.controllers
+package controllers
 
 import controllers.Assets
 import javax.inject.{Inject, Singleton}
