39096081ab1e2af64597941bca1d380ec718c43a
ajchilts <40767309+ajchilts@users.noreply.github.com>
2021-01-08 10:42:41 +0000

    Add config driven api configuarables for status and enabled

--- app/uk/gov/hmrc/individualsdetailsapi/controllers/APIDocumentationController.scala
+++ app/uk/gov/hmrc/individualsdetailsapi/controllers/APIDocumentationController.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 HM Revenue & Customs
+ * Copyright 2021 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -36,8 +36,21 @@ class APIDocumentationController @Inject()(cc: ControllerComponents,
         "api.access.version-P1.0.whitelistedApplicationIds")
       .getOrElse(Seq.empty)
 
+  private lazy val endpointsEnabled: Boolean =
+    config
+      .getOptional[Boolean]("api.access.version-1.0.endpointsEnabled")
+      .getOrElse(true)
+
+  private lazy val status: String =
+    config
+      .getOptional[String]("api.access.version-1.0.status")
+      .getOrElse("BETA")
+
   def definition(): Action[AnyContent] = Action { _ =>
-    Ok(txt.definition(privilegedWhitelistedApplicationIds))
+    Ok(
+      txt.definition(privilegedWhitelistedApplicationIds,
+                     endpointsEnabled,
+                     status))
       .withHeaders(CONTENT_TYPE -> JSON)
   }
 
