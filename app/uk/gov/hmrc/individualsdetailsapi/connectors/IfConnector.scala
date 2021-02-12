7c9e4b3848675e4e6cb16275bcb800e0b0752d58
ajchilts <40767309+ajchilts@users.noreply.github.com>
2021-02-12 11:46:35 +0000

    Update IfConnector.scala

--- app/uk/gov/hmrc/individualsdetailsapi/connectors/IfConnector.scala
+++ app/uk/gov/hmrc/individualsdetailsapi/connectors/IfConnector.scala
@@ -102,13 +102,13 @@ class IfConnector @Inject()(
       Future.failed(new InternalServerException("Something went wrong."))
     }
     case notFound: NotFoundException => {
+      auditHelper.auditIfApiFailure(correlationId, None, matchId, request, requestUrl, notFound.getMessage)
+      
       notFound.message.contains("PERSON_NOT_FOUND") match {
         case true => {
-          auditHelper.auditIfApiFailure(correlationId, None, matchId, request, requestUrl, notFound.getMessage)
           Future.successful(emptyResponse)
         }
         case _ => {
-          auditHelper.auditIfApiFailure(correlationId, None, matchId, request, requestUrl, notFound.getMessage)
           Future.failed(notFound)
         }
       }
