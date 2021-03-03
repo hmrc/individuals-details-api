d0cf57f221f668ac467b90a1cb1256d07426df93
owengilfellon <owen.gilfellon@opencastsoftware.com>
2021-03-03 16:33:30 +0000

    MDS-523 | fixed spacing

--- app/uk/gov/hmrc/individualsdetailsapi/audit/models/ApiResponseEventModel.scala
+++ app/uk/gov/hmrc/individualsdetailsapi/audit/models/ApiResponseEventModel.scala
@@ -49,16 +49,15 @@ object ResidencesApiResponseEventModel {
   implicit val formatApiResponseEventModel = Json.format[ResidencesApiResponseEventModel]
 }
 
-case class ApiResponseEventModel(
-                                           deviceId: String,
-                                           input: String,
-                                           method: String,
-                                           userAgent: String,
-                                           apiVersion: String,
-                                           matchId: String,
-                                           correlationId: Option[String],
-                                           scopes: String,
-                                           returnLinks: String)
+case class ApiResponseEventModel(deviceId: String,
+                                 input: String,
+                                 method: String,
+                                 userAgent: String,
+                                 apiVersion: String,
+                                 matchId: String,
+                                 correlationId: Option[String],
+                                 scopes: String,
+                                 returnLinks: String)
 
 object ApiResponseEventModel {
   implicit val formatApiResponseEventModel = Json.format[ApiResponseEventModel]
