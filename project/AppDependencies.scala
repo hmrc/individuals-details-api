a1340e453c796320e1b7b7d9b877fba089e79d9a
johno1985 <1640705+johno1985@users.noreply.github.com>
2023-10-25 18:32:39 +0100

    DLS-8994: Tidied up compile dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,24 +1,23 @@
 import play.sbt.PlayImport.ws
-import sbt._
+import sbt.*
 
 object AppDependencies {
 
   val hmrc = "uk.gov.hmrc"
-  val hmrcMongo = "uk.gov.hmrc.mongo"
+  val hmrcMongo = s"$hmrc.mongo"
   val hmrcMongoVersion = "1.3.0"
   val hmrcBootstrapVersion = "5.25.0"
 
-  val compile = Seq(
+  val compile: Seq[ModuleID] = Seq(
     ws,
     hmrc                %% "bootstrap-backend-play-28"  % hmrcBootstrapVersion,
     hmrc                %% "domain"                     % "8.3.0-play-28",
     hmrc                %% "play-hal"                   % "3.4.0-play-28",
-    hmrc                %% "play-hmrc-api"              % "7.1.0-play-28",
     hmrc                %% "json-encryption"            % "5.2.0-play-28",
     hmrcMongo           %% "hmrc-mongo-play-28"         % hmrcMongoVersion,
   )
 
-  def test(scope: String = "test,it") = Seq(
+  def test(scope: String = "test, it"): Seq[ModuleID] = Seq(
     "org.scalatestplus.play" %% "scalatestplus-play"       % "5.1.0"              % scope,
     "org.scalatestplus"      %% "mockito-3-4"              % "3.2.1.0"            % scope,
     "org.scalatestplus"      %% "scalacheck-1-17"          % "3.2.16.0"           % scope,
